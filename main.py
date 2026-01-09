449c25714317d5770d1fa7296eab94256c9a9dae
winzerprince <aitajprince200@gmail.com>
2026-01-09 09:35:06 +0300

    feat:Added fast api routes for each html file

--- main.py
+++ main.py
@@ -3,10 +3,29 @@ from fastapi.responses import HTMLResponse
 import pathlib
 
 app = FastAPI()
-index_path = pathlib.Path("public/index")
+
+index_path = pathlib.Path("public/index.html")
+acoustic_path = pathlib.Path("public/acoustic.html")
+electric_path = pathlib.Path("public/electric.html")
+bass_path = pathlib.Path("public/bass.html")
 
 @app.get("/")
 def server_index(response_class=HTMLResponse):
     content = index_path.read_text()
-    return HTMLResponse(content=content)
+    return response_class(content=content)
+
+@app.get("/acoustic")
+def server_acoustic(response_class=HTMLResponse):
+    content = acoustic_path.read_text()
+    return response_class(content=content)
+
+@app.get("/electric")
+def serve_electric(response_class=HTMLResponse):
+    content = electric_path.read_text()
+    return response_class(content=content)
+
+@app.get("/bass")
+def serve_bass(response_class= HTMLResponse):
+    content = bass_path.read_text()
+    return response_class(content=content)
     
